45de36ea3b89981b30f79766513d3643accaa363
root <root@ip-172-31-32-207.ec2.internal>
2016-10-08 16:22:44 +0000

    removing confidential info

--- wp-config.php
+++ wp-config.php
@@ -19,10 +19,10 @@
 define('DB_NAME', 'wordpress');
 
 /** MySQL database username */
-define('DB_USER', 'root');
+define('DB_USER', '');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'sabrina123');
+define('DB_PASSWORD', '');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
